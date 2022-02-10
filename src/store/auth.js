a377167feff5d4af47d7b75da9bffed81d976b30
weronikakurczyna <56171356+weronikakurczyna@users.noreply.github.com>
2022-02-10 01:03:30 +0100

    Update auth.js

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,7 +1,7 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 
 import {
-    deleteUserImageApiCall, getAllUsersEmailApiCall, getAllUsersEmailsApiCall,
+    deleteUserImageApiCall, getAllUsersEmailsApiCall,
     getAnotherUserApiCall, getMeApiCall,
     getUserApiCall,
     loginUserApiCall, registerUserApiCall, updatePasswordApiCall, updateUserApiCall, updateUserImageApiCall,
